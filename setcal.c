5b64960b51e4db79eee51dae85f484187c08fe11
mariantaragel <89635074+mariantaragel@users.noreply.github.com>
2021-11-16 19:59:37 +0100

    Update setcal.c

--- setcal.c
+++ setcal.c
@@ -1,21 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
+typedef struct{
     char **elements;
     int size;
     int capacity;    
 } Set;
 
+void set_ctor(Set *set)
+{
+    set->elements = NULL;
+    set->size = 0;
+    set->capacity = 0;
+}
+
+int read_set(char *filename)
+{
+    FILE *file = fopen(filename, "r");
+    if (file == NULL){
+        fprintf(stderr, "Chyba!\n");
+        return 0;
+    }
+
+    char c;
+    while (c != '\n'){
+        char element[31];
+        int i;
+        for (i = 0; (c = getc(file)) != (char)32; i++){
+            if (i >= 30){
+                fprintf(stderr, "Chyba!\n");
+                return 0;
+            }
+
+            element[i] = c;
+            if(c == '\n'){
+                break;
+            }
+        }
+        element[i] = '\0';
+
+        printf("%s", element);
+    }
+
+    fclose(file);
+
+    return 1;
+}
 
 int main(int argc, char **argv)
 {
     if (argc != 2){
-        fprintf(stderr, "Chyba!\n");
+        fprintf(stderr, "Chyba 1!\n");
         return EXIT_FAILURE;
     }
 
     Set universe;
+    set_ctor(&universe);
+    if (!(read_set(argv[1]))){
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
